4bc176343eea21721b14af317c9f23f43cf62639
Adarsh Kumar Singh <95336274+034adarsh@users.noreply.github.com>
2023-02-18 23:21:23 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a, b, c, d;
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+    c = a[0];
+    d = b[0];
+    a.erase(a.begin() + 0);
+    b.erase(b.begin() + 0);
+    cout<<d+a<<" "<<c+b;
+    return 0;
+}
 
